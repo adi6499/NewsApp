19b7ea893f194c2e0a9718c9133b3f23552a4851
adi6499 <83864698+adi6499@users.noreply.github.com>
2022-03-25 10:34:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,28 +5,37 @@ let World =  document.getElementById("world")
 
 
        async function raw(){
-            let urlLink = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=5f16d58af027486cb98c941f5d6db33e`;
+            let urlLink = `https://newsdata.io/api/1/news?apikey=pub_577240758f86c3ad08fac83f64be9f10618b&q=news&country=in,ru,ua `;
             let jsonData = await fetch(urlLink)
             let data = await jsonData.json()
-            data.articles.map((d)=>{
-                let {author,content,description,publishedAt,title,url,urlToImage} = d;
-              
- console.log(d)
-                news.innerHTML+=`
-            <a href=${url}><div class="box">
-                <img src=${urlToImage} class="newsImage"  alt="image"/>
-                <div class="title">${title}</div>
-                <div class="content">${content}</div>
-                <div class="date">${publishedAt}</div>
-            </div></a>
-            `
+            console.log(data.results)
+            data.results.map((d)=>{
+                let {author,content,full_description,pubDate,title,link,image_url} = d;
             
+                if(image_url==null){
+                   image_url=" http://writtent.com/blog/wp-content/uploads/2014/04/How-to-write-damn-good-headlines.jpg"
+            
+                }else if(full_description==undefined){
+                    full_description="No Info"
+                }
+                news.innerHTML+=`
+            <a href=${link}><div class="box">
+                <img src=${image_url} class="newsImage"  alt="image"/>
+                <div class="title">${title}</div>
+                <div class="content">${full_description}</div>
+                <div class="date">${pubDate}</div>
+            </div></a>
+             `
+            let image = document.querySelectorAll(".newsImage")
+          
            
              
             })
             
        }
        raw()  
+
+        
 
         
         
