8464adac6176151f97e71ff126ebd77fc68aa4a0
adi6499 <83864698+adi6499@users.noreply.github.com>
2022-03-25 09:53:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let World =  document.getElementById("world")
         let news = document.querySelector(".news")
-        let search = document.getElementById("search")
+       
 
 
 
-        search.addEventListener("click",async()=>{
-            let urlLink = `https://newsapi.org/v2/top-headlines?country=${World.value}&category=business&apiKey=5f16d58af027486cb98c941f5d6db33e`;
+       async function raw(){
+            let urlLink = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=5f16d58af027486cb98c941f5d6db33e`;
             let jsonData = await fetch(urlLink)
             let data = await jsonData.json()
             data.articles.map((d)=>{
@@ -25,9 +25,10 @@ let World =  document.getElementById("world")
              
             })
             
-          
+       }
+       raw()  
 
-        })
+        
         
    
         
